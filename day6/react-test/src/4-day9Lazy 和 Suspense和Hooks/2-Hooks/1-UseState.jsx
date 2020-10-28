e1f4163473dc5ea2495cb897e34e5facc381016e
xiaguikun <2973738150@qq.com>
2020-10-28 10:29:09 +0800

    dasdewas

--- day6/react-test/src/4-day9Lazy 和 Suspense和Hooks/2-Hooks/1-UseState.jsx
+++ day6/react-test/src/4-day9Lazy 和 Suspense和Hooks/2-Hooks/1-UseState.jsx
@@ -2,7 +2,7 @@ import React , { useState } from 'react';
 
 //引入useState，结构出一个数组[data,setData] =useState({state数据}) 
 //通过data.来获得数据
-//通过setState()来修改数据  接收一个函数作为参数，函数接收prevState作为参数，return{...prevState,count:1}，也可以直接接受一个对象修改数据
+//通过setState()来修改数据  接收一个函数作为参数，函数接收prevState作为参数，return{...prevState,count:1}，也可以直接接受一个对象修改数据{...data,count:1}
 
 const UseState = () => {
     let [data,setData] =useState({
